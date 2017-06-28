a3b423c0460d741897efd908392418ac5169f614
thkocks <timhkocks@gmail.com>
2017-06-28 16:09:30 +0200

    Add student views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   root to: 'pages#home'
 
   devise_for :users
 
-  resources :users, only: [show]
-  resources :students
-  resources :batches
-  resources :days
 end
